a57d58bbc7241634face1df1cc407fbe054453f3
AntonChanin <antonhaninvl@gmail.com>
2023-09-14 09:40:59 +0300

    стили кнопок

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+
+import './App.scss';
 
 function App() {
-  return (
-    <div></div>
-  );
+    return (
+        <div>
+            <div className="container">
+                <div className="problem">
+                    <button className="btn" id="interval">Показать с интервалом</button>
+                    <button className="btn" id="rxjs">Показать с RxJS</button>
+                    <div className="result" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
